f97eb9c079963791aa9d5718507098b1a5dc96e1
Ryo Kita <r.kita.09@gmail.com>
2019-05-05 18:47:41 +0900

    Adjust comment in topic_gen.py

--- recomend/topic_gen.py
+++ recomend/topic_gen.py
@@ -1,10 +1,11 @@
 import gensim
 
 def topic_gen(documents):
+    ## Load model
     lda_model = gensim.models.ldamodel.LdaModel.load('recomend/lda.model')
     dictionary = lda_model.id2word
     
-    ## Prediction
+    ## Transform docs into corpus
     texts = [[word for word in document.lower().split()] for document in documents]
     corpus = [dictionary.doc2bow(text) for text in texts]
     
@@ -12,9 +13,12 @@ def topic_gen(documents):
     for topic_prob in lda_model[corpus]:
         print(topic_prob)
 
+    ## Topic prediction and sort
     topic_prob = list(lda_model[corpus])[0]
     topic_prob.sort(key=lambda x: x[1], reverse=True)
     topic_line_up = [l[0] for l in topic_prob]
+
+    ## Return
     print(topic_line_up)
     
 
